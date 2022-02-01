d30654af497e9c2c313e20492c742d4f03fc72a9
muhammad zeon <73699852+zeon-X@users.noreply.github.com>
2022-02-02 03:01:22 +0600

    first commit

---
+++ class-programme.js
@@ -0,0 +1,172 @@
+
+// data structure array start
+const dsMondol = [
+    {
+        id: 1,
+        title: "CSE 1201 DS Mondol Sir Class 1",
+        link: "https://www.youtube.com/embed/5eghJHg7NwM",
+    },
+    {
+        id: 2,
+        title: "CSE 1201 DS Mondol Sir Class 2",
+        link: "https://www.youtube.com/embed/UelizuWbFnc",
+    },
+    {
+        id: 3,
+        title: "CSE 1201 DS Mondol Sir Class 3",
+        link: "https://www.youtube.com/embed/8IISemdH53w",
+    },
+    {
+        id: 4,
+        title: "CSE 1201 DS Mondol Sir Class 4",
+        link: "https://www.youtube.com/embed/OSqN0LH4KsE",
+    },
+    {
+        id: 5,
+        title: "CSE 1201 DS Mondol Sir Class 5",
+        link: "https://www.youtube.com/embed/vOtDqAOTUUo",
+    },
+    {
+        id: 6,
+        title: "CSE 1201 DS Mondol Sir Class 6",
+        link: "https://www.youtube.com/embed/OkSlWjcOp_I",
+    },
+    {
+        id: 7,
+        title: "CSE 1201 DS Mondol Sir Class 7",
+        link: "https://www.youtube.com/embed/K_hWn2mOfWc",
+    },
+    {
+        id: 8,
+        title: "CSE 1201 DS Mondol Sir Class 8",
+        link: "https://www.youtube.com/embed/r_Ezpffj9Dw",
+    },
+    {
+        id: 9,
+        title: "CSE 1201 DS Mondol Sir Class 9",
+        link: "https://www.youtube.com/embed/hVu3UcNwGCc",
+    },
+    {
+        id: 10,
+        title: "CSE 1201 DS Mondol Sir Class 10",
+        link: "https://www.youtube.com/embed/gnMc7dEPxWA",
+    },
+    {
+        id: 11,
+        title: "CSE 1201 DS Mondol Sir Class 11",
+        link: "https://www.youtube.com/embed/9_AuLbh3BMU",
+    },
+    {
+        id: 12,
+        title: "CSE 1201 DS Mondol Sir Class 12",
+        link: "https://www.youtube.com/embed/D_mg0YEEY4c",
+    },
+    {
+        id: 13,
+        title: "CSE 1201 DS Mondol Sir Class 13",
+        link: "https://www.youtube.com/embed/A6X_e_tR96I",
+    },
+
+];
+const dsRT = [
+    {
+        id: 1,
+        title: "CSE 1201 DS RT Sir Class 1",
+        link: "https://www.youtube.com/embed/IX_hfW5vffI",
+    },
+    {
+        id: 2,
+        title: "CSE 1201 DS RT Sir Class 2",
+        link: "https://www.youtube.com/embed/c30sCkSG9lo",
+    },
+    {
+        id: 3,
+        title: "CSE 1201 DS RT Sir Class 3",
+        link: "https://www.youtube.com/embed/8hgx7qQm6UM",
+    },
+    {
+        id: 4,
+        title: "CSE 1201 DS RT Sir Class 4",
+        link: "https://www.youtube.com/embed/rBaWoKr7jrk",
+    },
+    {
+        id: 5,
+        title: "LAB CSE 1202 DS RT Sir Class 5",
+        link: "https://www.youtube.com/embed/U31ePpvRn3s",
+    },
+    {
+        id: 6,
+        title: "CSE 1201 DS RT Sir Class 6",
+        link: "https://www.youtube.com/embed/EUaMoH-lPz4",
+    },
+    {
+        id: 7,
+        title: "CSE 1201 DS RT Sir Class 7",
+        link: "https://www.youtube.com/embed/5BIeEKHaZKQ",
+    },
+    {
+        id: 8,
+        title: "LAB CSE 1202 DS RT Sir Class 8",
+        link: "https://www.youtube.com/embed/AG8ylmoOke0",
+    },
+    {
+        id: 9,
+        title: "CSE 1201 DS RT Sir Class 9",
+        link: "https://www.youtube.com/embed/dr4tnPBkk4U",
+    },
+    {
+        id: 10,
+        title: "LAB CSE 1202 DS RT Sir Class 10",
+        link: "https://www.youtube.com/embed/8ZG_IUhlN2U",
+    },
+    {
+        id: 11,
+        title: "CSE 1201 DS RT Sir Class 11",
+        link: "https://www.youtube.com/embed/CFwFq3wzd-0",
+    },
+    {
+        id: 12,
+        title: "CSE 1201 DS RT Sir Class 12",
+        link: "https://www.youtube.com/embed/3qU0TpJZvak",
+    },
+    {
+        id: 13,
+        title: "LAB CSE 1202 DS RT Sir Class 13",
+        link: "https://www.youtube.com/embed/jYmKMG4hZeQ",
+    },
+    {
+        id: 14,
+        title: "CSE 1201 DS RT Sir Class 13",
+        link: "https://www.youtube.com/embed/tObKkn2Cgc",
+    },
+
+];
+const ds = [
+    [dsMondol, dsRT]
+];
+// data structure array end
+
+
+
+// oop array start
+
+// oop array end
+
+
+// Mathematics array start
+// Mathematics array end
+
+
+// eco array start
+// eco array end
+
+// Physics array start
+// Physics array end
+
+
+const firstyearevensem = [
+    ds,
+    oop
+];
+
+console.log(firstyearevensem);
